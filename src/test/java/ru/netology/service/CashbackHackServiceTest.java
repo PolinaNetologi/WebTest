21c9406d8012f53eb62d6b1dbbbf9a7aa69bc621
Polina <pzmujdeckaa@gmail.com>
2023-06-26 22:47:15 +0500

    Testng

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -4,7 +4,7 @@ package ru.netology.service;
 
 import static org.testng.Assert.assertEquals;
 public class CashbackHackServiceTest {
-    @org.testng.annotation.Test
+    @org.testng.annotations.Test
     public void testRemain() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
@@ -13,8 +13,8 @@ public class CashbackHackServiceTest {
         assertEquals(actual, expected);
     }
 
-    @org.testng.annotation.Test
-    public void testRemain() {
+    @org.testng.annotations.Test
+    public void testRemain1() {
         CashbackHackService service = new CashbackHackService();
         int amount = 900;
         int actual = service.remain(amount);
@@ -22,6 +22,30 @@ public class CashbackHackServiceTest {
         assertEquals(actual, expected);
 
 
+    }
+
+    @org.testng.annotations.Test
+
+    public void testRemain2() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+
+    public void testRemain3() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1200;
+        int actual = service.remain(amount);
+        int expected = 800;
+
+        assertEquals(actual, expected);
     }
 }
 
