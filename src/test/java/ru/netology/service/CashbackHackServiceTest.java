17a67a59d21839c18e866a2e5b55173d15d1e5a2
Polina <pzmujdeckaa@gmail.com>
2023-06-27 18:28:14 +0500

    Junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -10,7 +10,7 @@ public class CashbackHackServiceTest {
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 
     @org.junit.Test
@@ -33,7 +33,7 @@ public class CashbackHackServiceTest {
         int actual = service.remain(amount);
         int expected = 1000;
 
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 
     @org.junit.Test
@@ -45,7 +45,7 @@ public class CashbackHackServiceTest {
         int actual = service.remain(amount);
         int expected = 800;
 
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 }
 
